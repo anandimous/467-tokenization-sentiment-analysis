b22d9bb357979b28ba7d1b961365364048c7e885
Aniruddha Nandi <anandi@buffalo.edu>
2018-11-09 12:44:18 -0500

    added trump tweet viz module, connected text o/p

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 Tokenization and Sentimental Anaylysis of Trump's tweets
 
+## Blog
+
+Compilation of progress made recorded & updated weekly [here] (https://github.com/anandimous/467-tokenization-sentiment-analysis/wiki)
+
+- [Week 0] (https://github.com/anandimous/467-tokenization-sentiment-analysis/wiki/Week-0)
+- [Week 1] (https://github.com/anandimous/467-tokenization-sentiment-analysis/wiki/Week-1)
+
 ## Team Members
 > Aniruddha Nandi
 
--- trump-tweets.R
+++ trump-tweets.R
@@ -78,3 +78,74 @@ dtm1 <- DocumentTermMatrix(docs1, control = list(wordLengths = c(3, 20)))## corp
 freq1 <- colSums(as.matrix(dtm1)) # org terms by freq
 freq2 <- as.data.frame(freq1)
 freq2$word <- row.names(freq2)
+
+## FEC Data ##
+fec <- read.csv("Trump.FEC.Data.Large.csv")
+fec$amount <- as.numeric(fec$amount)
+fec$Date <-as.Date(fec$Date, "%m/%d/%Y")
+fec <- fec %>% group_by(committee_name) %>% mutate(total_amount = cumsum(amount)) %>% as.data.frame() # create running total all the way down
+fec$total_amount <- as.numeric(fec$total_amount)
+fec[1:8,10:12] <- format(as.character(fec[1:8,10:12]))
+
+#ap <- Approval.Raiting
+#ap$Date <- as.Date(ap$Date, "%m/%d/%Y")
+
+## Random Variables ##
+j <- as.Date("1970-01-01")
+pal = brewer.pal(8,"Dark2")
+
+ui <- fluidPage(
+   dateRangeInput(inputId = "date1", 
+                 strong("Date Range"),
+                 start = "2017-03-01", end = "2018-03-01",
+                 min = "2009-05-01", max ="2018-03-01"),  
+  fluidRow(
+    splitLayout(cellWidths = c("50%", "50%"),
+    plotOutput("TrumpGraph",
+             click = "plotClick")))
+)
+
+## Things to Add:
+##   1. Create a CORPUS of Trumps tweets (e.g. Wordcount, sentiment analysis, possibly co-occurence), load from vector
+##       a. lowercase, strip: white space, special characters, stemming(?), numbers, punctuation
+##   2. Create another text box that will output a WORD COUNT of words in that tweet of words used over the whole corpus
+##       a. renderTable(?)
+##   3. Create SENTIMENT ANALYSIS graph:
+##       a. Tweet: overwhemeling pos/neg
+##       b. Of whole corpus, possible line graph of positive/negative over TPD
+##   4. WordCloud (of corpus/tweet?)
+##   5. Connect words (in corpus/tweets) to donations (HOW???)
+##   6. Change in sentiment toward certian things
+##       a. add search bar and plot results (e.g. - guns)   
+
+server <- function(input, output) {
+  
+  output$TrumpGraph <- renderPlot({
+    ggplot(tweets2, aes(x = Date, y = count, fill = Total)) +        
+      geom_bar(stat = "identity", position = "stack") +
+      #geom_line(data = ap, aes(x = Date, y = Trump, fill = Trump)) +
+      #scale_y_continuous(name = "Tweets Per Day", sec.axis = (~. *100 / 160)) +
+      scale_x_date(limits = c(input$date1[1], input$date1[2])) +  ###### X-Axis for reactive date range (REACTIVITY WON'T WORK WITHOUT THIS) #######
+      scale_fill_gradient(low="yellow", high="red") + 
+      theme(panel.background = element_rect(fill = "grey"), axis.text.x = element_text(face = "bold"))
+  })
+
+  ## Tweet text click output ##
+  output$click_info <- renderText({
+      t <- input$plotClick$x
+      xx <- input$plotClick$x
+      t <- as.character(t)
+      yy <- strsplit(t, "\\.")
+      z <- yy[[1]][[2]]
+      z <- substr(z, 1, 1)
+      z <- as.numeric(z)
+      x <- as.numeric(t)
+      if (z <= 5) {x <- x - 1}
+      x <- floor(as.numeric(x))
+      y <- input$plotClick$y
+      y <- ceiling(as.numeric(y))
+      d <- which((tweets2$rando == x) & (tweets2$Total == y), arr.ind = TRUE)
+      d <- as.numeric(d)
+      i <- tweets2$text[d]
+      HTML("Tweet:\n",i) 
+    })
